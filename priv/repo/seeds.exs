a3efacd06d08aeeabf6950717fe92fba0e3c8f08
Todd Morningstar <techarchist@gmail.com>
2018-02-28 16:06:54 -0500

    Add Video seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,3 +1,4 @@
+###############################################################################
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
@@ -9,3 +10,25 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+###############################################################################
+
+alias FireStarter.{Repo,Video}
+
+### Videos ###
+
+# Delete all prior records before seeding data.
+Repo.delete_all(Video)
+
+# Add two records.
+elixir = %Video{title: "Elixir 101", duration: 120}
+javascript = %Video{title: "Advanced Javascript", duration: 300}
+
+# The basic way to add the above records is via a `Repo.insert/1` call:
+#  - `Repo.insert(elixir)`
+#  - `Repo.insert(javascript)`
+#
+# The following use of `Enum` is more Elixir idiomatic:
+#  - Use of the `Enum` module for handling procedural iterations
+#  - Use of the anonymous function for the insertion (via `Enum.each/2`)
+#
+Enum.each([elixir, javascript], &( Repo.insert(&1) ))

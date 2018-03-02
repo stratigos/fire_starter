536e997b77ed0453c8d5ef8f84ed94f9d7769fec
stratigos <stratigos@users.noreply.github.com>
2018-03-02 13:41:40 -0500

    Create webform submission for new Video
     * Implement flash messages in VideoController
     * Implement routine for saving Video record
     * Add documentation

--- lib/fire_starter/video.ex
+++ lib/fire_starter/video.ex
@@ -11,6 +11,11 @@ defmodule FireStarter.Video do
     timestamps()
   end
 
+  # Using Ecto, create a record in the data store by first casting its
+  #  attributes (which are most likely Strings) according to the column types
+  #  as defined in `schema/1` above, run validations on attribute values as
+  #  needed, then return the Video struct.
+  # This boilerplate updating routine is created via Ecto migration CLI.
   @doc false
   def changeset(%Video{} = video, attrs) do
     video
--- lib/fire_starter_web/controllers/video_controller.ex
+++ lib/fire_starter_web/controllers/video_controller.ex
@@ -10,7 +10,47 @@ defmodule FireStarterWeb.VideoController do
   end
 
   def new(conn, _) do
-    render(conn, "new.html")
+    # Since this is a new record, the changeset consists of an empty Struct,
+    #  with an empty Map applied to it (as opposed to a key-val of some sort
+    #  with attributes and values).
+    # @see Video.changeset/2
+    changeset = Video.changeset(%Video{}, %{})
+
+    # Pass this empty changeset to the template.
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"video" => video_params}) do
+    # Since this is the POST-ed data matching the request after `new/2` above,
+    #  the `changeset` is now an empty, or _new_ Struct. The key-val (a Map,
+    #  created from pattern matching against the request params passed to
+    #  `create/2`) argument now contains attributes and designated values.
+    changeset = Video.changeset(%Video{}, video_params)
+
+    # Create the record.
+    # On success, return a standard Elixir tuple containing the record. Display
+    #  a flash message to the user.
+    # On fail, load the changeset into the template for `new/2` and re-render.
+    #  Display a failure message to the user.
+    case Repo.insert(changeset) do
+      # On success, the Video record would be returned, but it isn't needed, so
+      #  it is replaced with the `_` char to denote it is being ignored. Then,
+      #  an anonymous function is called which handles setting the flash msg
+      #  and redirecting back to the index page, within the context of the
+      #  current connection.
+      # On fail, the Video record's requested data is relevant, so the tuple
+      #  contains a pattern-matched Map of attrs and vals. Just like above, an
+      #  anon func is created, and the current connection's flash and rendering
+      #  are handled.
+      {:ok, _} ->
+        conn
+          |> put_flash(:info, "Video created successfully!")
+          |> redirect(to: video_path(conn, :index))
+      {:error, changeset} ->
+        conn
+          |> put_flash(:error, "Please check the form for errors.")
+          |> render("new.html", changeset: changeset)
+    end
   end
 
 end
